731179648a37dae71ee5fb5be84e2f5465549785
Bernard O'Leary <bernard.oleary@gmail.com>
2017-04-21 00:53:09 +1200

    Added .gitignore entries for node_modules and .vscode, and added node solution skeleton for Developer Lab 1.

---
+++ app.js
@@ -0,0 +1,44 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder');
+var restify = require('restify');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create connector and listen for messages
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Computer says no. Dunno what \'%s\' means. Type \'help\' if you like.', session.message.text);
+});
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('Book Leave', function (session, args) {
+    // retrieve type of leave name from matched entities
+    var typeOfLeaveEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Type of Leave');
+    if (typeOfLeaveEntity) {
+        session.send('You want to book \'%s\' leave...?', typeOfLeaveEntity.entity);
+    }
+}).triggerAction({
+    matches: 'Book Leave'
+});
+
+bot.dialog('Help', function (session) {
+    session.endDialog('Computer still says no. Sorry.');
+}).triggerAction({
+    matches: 'Help'
+});
